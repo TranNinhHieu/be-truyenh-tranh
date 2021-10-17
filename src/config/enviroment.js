176687fbe7922ec9f97a58ba5e77a35caa498687
river248 <85404917+river248@users.noreply.github.com>
2021-10-18 02:07:14 +0700

    Config mongodb, use environment variable.

---
+++ src/config/enviroment.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+export const env = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    DATABASE_NAME: process.env.DATABASE_NAME
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 import express from 'express'
+import { connectDB } from './config/mongodb'
+import { env } from './config/enviroment'
 
 const app = express()
 
-const hostname = 'localhost'
-const port = 8080
+connectDB().catch(console.log)
 
 app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr/>')
 })
 
-app.listen(port, hostname, () => {
-     console.log(`Hello river, I'm running at ${hostname}:${port}/`)
+app.listen(env.PORT, env.HOST, () => {
+     console.log(`Hello river, I'm running at ${env.HOST}:${env.PORT}/`)
 })
