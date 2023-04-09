279c496b038ac0aa6f2df430300bc32e856f7170
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2023-04-10 01:40:34 +0700

    oke

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const bootServer = () => {
             credentials: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             origin: [
-                'https://comic-riverdev-api.herokuapp.com',
+                'https://be-truyenh-tranh.onrender.com/',
                 'http://localhost:3000',
                 'https://comic-riverdev-web.web.app',
                 'http://localhost:8080']
@@ -46,14 +46,14 @@ const bootServer = () => {
             credentials: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             origin: [
-                'https://comic-riverdev-api.herokuapp.com',
+                'https://be-truyenh-tranh.onrender.com/',
                 'http://localhost:3000',
                 'https://comic-riverdev-web.web.app',
                 'http://localhost:5000']
         }
     })
 
-    httpServer.listen(5000, () => {
+    httpServer.listen(process.env.PORT, () => {
         console.log(`Hello river, I'm running at port: ${5000}/`)
     })
 
