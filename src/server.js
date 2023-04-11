2430ec147a48d3e273fc3dd9861009c9965b6ec7
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2023-04-11 15:01:27 +0700

    ád

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 import { connectDB } from './config/mongodb'
 import { env } from './config/enviroment'
 import { apiV1 } from './routes/v1'
-import { corsConfig } from './config/cors'
+import cookieParse from 'cookie-parser'
 
 connectDB()
     .then(() => console.log('Connected successfully to database server!'))
@@ -14,15 +16,56 @@ connectDB()
     })
 
 const bootServer = () => {
+
     const app = express()
 
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(cors(corsConfig))
+    app.set('trust proxy', 1)
+    app.use(cookieParse())
+    app.use(
+        cors({
+            credentials: true,
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            origin: [
+                'https://be-truyenh-tranh.onrender.com',
+                'http://localhost:3000',
+                'https://comic-riverdev-web.web.app',
+                'http://localhost:8080']
+        })
+    )
 
+    // Enable req.body data
+    app.use(express.json())
+
+    app.use(express.urlencoded({ extended: true }))
+    // Use APIs v1
     app.use('/v1', apiV1)
 
-    app.listen(env.APP_PORT || process.env.PORT, () => {
-        console.log(`Hello Comic, I'm running at :${process.env.PORT}/`)
+    const httpServer = createServer(app)
+    const io = new Server(httpServer, {
+        cors: {
+            credentials: true,
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            origin: [
+                'https://be-truyenh-tranh.onrender.com',
+                'http://localhost:3000',
+                'https://comic-riverdev-web.web.app',
+                'http://localhost:5000']
+        }
+    })
+
+    httpServer.listen(5000, () => {
+        console.log(`Hello river, I'm running at port: ${5000}/`)
+    })
+
+    io.on('connection', (socket) => {
+
+        console.log('someone has connected!')
+        socket.on('disconnect', () => {
+            console.log('someone has left!')
+        })
+
+        socket.on('client-like-comic', (data) => {
+            socket.emit('server-like-comic', data)
+        })
     })
 }
